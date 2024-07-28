fcf433ff14c0585f87045688341d5a59e9af5816
AkKiL M G <78695802+AkkilMG@users.noreply.github.com>
2024-07-28 23:37:11 +0530

    No idea bro

--- Best/7.py
+++ Best/7.py
@@ -1,4 +1,8 @@
 import pandas as  pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
 
 
 msg=pd.read_csv('naivetext.csv',names=['message','label'])
@@ -9,12 +13,10 @@ y=msg.labelnum
 print(X)
 print(y)
 
-from sklearn.model_selection import train_test_split
 Xtrain,Xtest,ytrain,ytest=train_test_split(X,y)
 print("\n The total no of training data",ytrain.shape)
 print("\n The total no of testing data",ytest.shape)
 
-from sklearn.feature_extraction.text import CountVectorizer
 count_vect=CountVectorizer()
 Xtrain_dtm=count_vect.fit_transform(Xtrain)
 Xtest_dtm=count_vect.transform(Xtest)
@@ -22,12 +24,9 @@ print("\n the words or token in text document\n")
 print(count_vect.get_feature_names())
 df=pd.DataFrame(Xtrain_dtm.toarray(),columns=count_vect.get_feature_names())
 
-from sklearn.naive_bayes import MultinomialNB
 clf=MultinomialNB().fit(Xtrain_dtm,ytrain)
 predicted=clf.predict(Xtest_dtm)
 
-
-from sklearn import metrics
 print("\n accuracy of classifier",metrics.accuracy_score(ytest,predicted))
 print("\n confusion matrix")
 print(metrics.confusion_matrix(ytest,predicted))
